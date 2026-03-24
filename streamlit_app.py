9fbb8de8991da6a0079b38d43ce44e55ac4f1b62
Shweta Kumari <shweta181005@gmail.com>
2026-03-24 20:25:14 +0530

    added UI improvements, badges, progress bar and plotly chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.graph_objects as go
 from utils import extract_text_from_pdf, clean_text, extract_skills, calculate_match, missing_skills
 from data.skills import skills_list
 
@@ -23,14 +24,58 @@ if uploaded_file and job_description:
 
     # --UI DISPLAY--
 
+    #Match score
     st.subheader("Match Score")
-    st.write(f"{round(score, 2)} %")
+    st.metric(label = "Your Resume Match", value = f"{round(score,2)}%")
+    st.progress(int(score))
 
-    st.subheader("Matching Skills")
-    st.write(list(matched))
+    #skills- two colums
+    col1, col2 = st.columns(2)
 
-    st.subheader("Skills in Resume")
-    st.write(resume_skills)
+    #helper func to render colored badges
+    def render_badges(skills, color):
+        badges = " ".join([
+            f"<span style = 'backgrounf-color:{color};color:white; padding:4px 10px; border-radius:12px; margin:3px; display:inline-block; font-weight:500'>{skill}</span>"
+            for skill in skills
+        ])
+        st.markdown(badges, unsafe_allow_html = True)
 
-    st.subheader("Missing skills (Improve These!)")
-    st.write(list(missing))
+    with col1:
+        st.subheader("Matched skills")
+        if matched:
+            render_badges(matched, "#90EE90")    
+
+    with col2:
+        st.subheader("Missing skills")
+        if missing:
+            render_badges(missing, "#A81212")    
+        else:
+            st.write("You have all required skills")      
+
+    #Resume skills summary at the bottom 
+    st.divider()
+    st.subheader("All skills found in your Resume")
+    render_badges(resume_skills, "#ADD8E6")  
+
+    st.subheader("📊 Skills Breakdown")
+
+    fig = go.Figure(data=[go.Pie(
+         labels=["Matched", "Missing"],
+        values=[len(matched), len(missing)],
+        hole=0.4,  # makes it a donut chart
+        marker=dict(colors=["#90EE90", "#A81212"])  # green, red
+    )])
+
+    fig.update_layout(
+        showlegend=True,
+        paper_bgcolor="rgba(0,0,0,0)",  # transparent background
+        font=dict(color="white")
+    )
+
+    st.plotly_chart(fig) 
+    
+     
+          
+
+
+
